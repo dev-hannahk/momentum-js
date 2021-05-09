f163114946d2dd3993cebc8f5d07ac0832005b6a
dev-hannahk <dev.hannahk@gmail.com>
2021-05-09 23:18:41 +0900

    Add: Saving the User Name
    Modify: README.md, style.css
    Add: reset.css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Clone Momentum Extension by Vanilla JS!
+## Momentum To Do App
+
+- This is based on Nomad Coders' [Vanilla JS tutorial for beginners](https://nomadcoders.co/javascript-for-beginners).
+- It's cloning the chrome extension ["Momentum"](https://chrome.google.com/webstore/detail/momentum/laookkfknpbbblfpciffpaejjkokdgca) made with Vaniila JS only.
--- clock.js
+++ clock.js
@@ -1,17 +1,19 @@
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle = clockContainer.querySelector("h1");
+const clockContainer = document.querySelector('.js-clock');
+const clockTitle = clockContainer.querySelector('h1');
 
 const getTime = () => {
   const data = new Date();
   const hours = data.getHours();
   const minutes = data.getMinutes();
   const seconds = data.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-}
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+};
 
-const init = () => {
+const initClock = () => {
   getTime();
   setInterval(getTime, 1000);
-}
+};
 
-init();
+initClock();
